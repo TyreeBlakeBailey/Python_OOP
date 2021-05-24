934e962cd348010e5b182b2d8dc20cb80525f274
Tyree <t9tyree@hotmail.co.uk>
2021-05-24 13:34:00 +0100

    Added Extra modules tests

--- Python OS.py
+++ Python OS.py
@@ -1,6 +1,10 @@
-import os, sys
+import os, sys, datetime, math
+print(os.cpu_count())
+print(datetime.datetime.now())
 
+print(math.remainder(93, 9))
 
+class System_path:
 
 working_dir = os.getcwd()
 print(f"This is your current working dir {working_dir}")
