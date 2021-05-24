d2c831e6a2af6126d750f568da5c3d00dd3e429c
Tyree <t9tyree@hotmail.co.uk>
2021-05-24 13:37:28 +0100

    Python OS.py added the value of pi using math.

--- Python OS.py
+++ Python OS.py
@@ -1,26 +1,29 @@
 import os, sys, datetime, math
-print(os.cpu_count())
-print(datetime.datetime.now())
+# print(os.cpu_count())
+# print(datetime.datetime.now())
+#
+# print(math.remainder(93, 9))
+#
+# #class System_path:
+#
+# working_dir = os.getcwd()
+# print(f"This is your current working dir {working_dir}")
+#
+# system_path = sys.path
+# print(f"This is your path {system_path}")
+#
+# def current_system_path():
+#     print("This is your path ")
+#     return sys.path
+#
+#
+# def working_dir():
+#     print("This is your current dir")
+#     return os.getcwd()
+#
+#
+# print(working_dir())
+# print(current_system_path())
 
-print(math.remainder(93, 9))
 
-class System_path:
-
-working_dir = os.getcwd()
-print(f"This is your current working dir {working_dir}")
-
-system_path = sys.path
-print(f"This is your path {system_path}")
-
-def current_system_path():
-    print("This is your path ")
-    return sys.path
-
-
-def working_dir():
-    print("This is your current dir")
-    return os.getcwd()
-
-
-print(working_dir())
-print(current_system_path())
+print(math.pi)
