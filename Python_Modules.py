522630a5b21aa64a39e5ecc78a379cb79b86fb33
Tyree <t9tyree@hotmail.co.uk>
2021-05-24 12:08:47 +0100

    Exercise rounding up inputted number(No error checking)

---
+++ Python_Modules.py
@@ -0,0 +1,36 @@
+import random
+import math
+
+
+# can also call "from random import random" that will allow you to only import teh funtion that you need
+# print(random.random())
+# # Will print out a random number
+#
+# num1 = 14.9
+# print(num1)
+# print(math.ceil(num1))  # will always round the given number up
+# print(math.floor(num1))  # Will also round thd number down
+# print(ran_num)
+
+def round(Num):
+    if math.modf(Num)[ 0 ] >= 0.50:
+        return math.ceil(Num)
+    elif math.modf(Num)[ 0 ] < 0.50:
+        return math.floor(Num)
+
+
+def IS_String(Input):
+    Input = float(Input)
+    if isinstance(Input, float):
+        return True
+    else:
+        return False
+
+
+while True:
+    User_Num = input("Enter a decimal number to round\n")
+    if IS_String(User_Num):
+        print(round(float(User_Num)))
+    else:
+        print("Please enter correct value")
+    print(f"The random generate number rounded is {round(random.random())}")
--- README.md
+++ README.md
@@ -7,5 +7,8 @@
 - API with requests
 
 
+## Modules
+Python modules are group of functions and tools that can be called by the user by using the key work import
+
 ### key Word 
 "Weekly sprint planning "
